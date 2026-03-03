a6be2980d831a2e4ae49c644bcb6724f3794ed34
Kavin9898 <kavindharan40828@gmail.com>
2026-03-04 00:03:58 +0530

    Update subnets.tf

--- infrastructure/subnets.tf
+++ infrastructure/subnets.tf
@@ -1,21 +1,45 @@
-resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "192.168.0.0/25"
-  availability_zone = "ap-south-1a"
-
+# PUBLIC SUBNET - AZ 1
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "192.168.0.0/26"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-subnet"
+    Name = "public-subnet-1"
   }
 }
 
-resource "aws_subnet" "private_subnet" {
+# PUBLIC SUBNET - AZ 2
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "192.168.0.64/26"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+# PRIVATE SUBNET - AZ 1
+resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "192.168.0.128/25"
+  cidr_block        = "192.168.0.128/26"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+# PRIVATE SUBNET - AZ 2
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "192.168.0.192/26"
   availability_zone = "ap-south-1b"
 
   tags = {
-    Name = "private-subnet"
+    Name = "private-subnet-2"
   }
 }
