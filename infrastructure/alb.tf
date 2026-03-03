e0d7304b176351f3d8e4393705f72d7a6effd9af
Kavin9898 <kavindharan40828@gmail.com>
2026-03-04 00:06:22 +0530

    Update alb.tf

--- infrastructure/alb.tf
+++ infrastructure/alb.tf
@@ -3,5 +3,13 @@ resource "aws_lb" "app_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.web_sg.id]
-  subnets            = [aws_subnet.public_subnet.id]
+
+  subnets = [
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.public_subnet_2.id
+  ]
+
+  tags = {
+    Name = "app-alb"
+  }
 }
