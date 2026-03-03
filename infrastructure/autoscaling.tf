0b2116fd616d38ff0b425c9d8f7105251862139a
Kavin9898 <kavindharan40828@gmail.com>
2026-03-04 00:07:07 +0530

    Update autoscaling.tf

--- infrastructure/autoscaling.tf
+++ infrastructure/autoscaling.tf
@@ -1,11 +1,21 @@
 resource "aws_autoscaling_group" "app_asg" {
-  desired_capacity     = 2
-  max_size             = 3
-  min_size             = 1
-  vpc_zone_identifier  = [aws_subnet.public_subnet.id]
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 1
+
+  vpc_zone_identifier = [
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_2.id
+  ]
 
   launch_template {
     id      = aws_launch_template.app_lt.id
     version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "app-instance"
+    propagate_at_launch = true
   }
 }
