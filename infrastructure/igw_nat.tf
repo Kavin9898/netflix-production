4126c73c07eb56579ad101286f3f7f482ef261ff
Kavin9898 <kavindharan40828@gmail.com>
2026-03-04 00:09:35 +0530

    Update igw_nat.tf

--- infrastructure/igw_nat.tf
+++ infrastructure/igw_nat.tf
@@ -1,12 +1,8 @@
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_eip" "nat_eip" {
-  domain = "vpc"
-}
-
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet.id
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "nat-gateway"
+  }
 }
