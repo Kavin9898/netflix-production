a8024252f295f00b0185b15dc17cb8fc32c0cb6f
Kavin9898 <kavindharan40828@gmail.com>
2026-03-04 00:15:34 +0530

    Update igw_nat.tf

--- infrastructure/igw_nat.tf
+++ infrastructure/igw_nat.tf
@@ -1,3 +1,13 @@
+# Internet Gateway (Required for Public Subnets)
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Elastic IP for NAT
 resource "aws_eip" "nat" {
   domain = "vpc"
 
@@ -5,6 +15,8 @@ resource "aws_eip" "nat" {
     Name = "nat-eip"
   }
 }
+
+# NAT Gateway (for Private Subnets Internet Access)
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public_subnet_1.id
