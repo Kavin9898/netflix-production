3f145bf54c9cfc7e54353fd60a24984e96bda200
Kavin9898 <kavindharan40828@gmail.com>
2026-03-03 23:27:43 +0530

    Update Movies.js

--- frontend/src/pages/Movies.js
+++ frontend/src/pages/Movies.js
@@ -1,28 +1,22 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
+import api from "../services/api";
+import MovieCard from "../components/MovieCard";
 
 function Movies() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:6000/api/movies")
-      .then(res => setMovies(res.data));
+    api.get("/movies")
+      .then((res) => setMovies(res.data))
+      .catch((err) => console.error(err));
   }, []);
 
   return (
     <div>
-      <h2>Movies</h2>
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        {movies.map(movie => (
-          <div key={movie._id} style={{ margin: "20px" }}>
-            <img src={movie.thumbnail} width="200" alt={movie.title} />
-            <h4>{movie.title}</h4>
-            <a href={movie.videoUrl} target="_blank" rel="noreferrer">
-              ▶ Play
-            </a>
-          </div>
-        ))}
-      </div>
+      <h2>Movies 🍿</h2>
+      {movies.map((movie) => (
+        <MovieCard key={movie._id} movie={movie} />
+      ))}
     </div>
   );
 }
