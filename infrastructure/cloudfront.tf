e5b08e5f20803f909dd11c2841cf70b7ba29d398
Kavin9898 <kavindharan40828@gmail.com>
2026-03-03 23:54:29 +0530

    Update cloudfront.tf

--- infrastructure/cloudfront.tf
+++ infrastructure/cloudfront.tf
@@ -1,24 +1,41 @@
 resource "aws_cloudfront_distribution" "cdn" {
+
   origin {
-    domain_name = aws_s3_bucket.app_bucket.bucket_regional_domain_name
-    origin_id   = "s3-origin"
+    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
+    origin_id   = "frontend-origin"
   }
 
-  enabled = true
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
 
   default_cache_behavior {
-    target_origin_id = "s3-origin"
+    target_origin_id       = "frontend-origin"
     viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
 
     forwarded_values {
       query_string = false
+
       cookies {
         forward = "none"
       }
     }
   }
 
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
   viewer_certificate {
     cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Name = "streamflix-cdn"
   }
 }
