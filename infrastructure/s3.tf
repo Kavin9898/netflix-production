150f76b52b85c47c06954d791bf3e835334ed179
Kavin9898 <kavindharan40828@gmail.com>
2026-03-03 23:56:33 +0530

    Update s3.tf

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,3 +1,12 @@
-resource "aws_s3_bucket" "app_bucket" {
-  bucket = "netflix-production"
+resource "aws_s3_bucket" "frontend" {
+  bucket = "streamflix-frontend-801195563235"
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
