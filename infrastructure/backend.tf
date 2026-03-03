65db0d4e803eecaca5ac0b5e389d0d549fa38a28
Kavin9898 <kavindharan40828@gmail.com>
2026-03-03 23:49:16 +0530

    Update backend.tf

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "netflix-production"
+    bucket = "netflix-production-801195563235"
     key    = "project/terraform.tfstate"
     region = "ap-south-1"
   }
