94319a000efb6cb46f15eeb34e481dc2feab2054
Kavin9898 <kavindharan40828@gmail.com>
2026-03-06 16:58:00 +0530

    Update rds.tf

--- infrastructure/rds.tf
+++ infrastructure/rds.tf
@@ -1,8 +1,26 @@
+resource "aws_db_subnet_group" "db_subnet" {
+  name = "db-subnet-group"
+
+  subnet_ids = [
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_2.id
+  ]
+
+  tags = {
+    Name = "db-subnet-group"
+  }
+}
+
 resource "aws_db_instance" "mysql" {
   allocated_storage    = 20
   engine               = "mysql"
-  instance_class       = "db.t3.micro"
+  instance_class       = "db.t3.medium"
   username             = var.db_username
   password             = var.db_password
   skip_final_snapshot  = true
+
+  db_subnet_group_name = aws_db_subnet_group.db_subnet.name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  publicly_accessible = false
 }
