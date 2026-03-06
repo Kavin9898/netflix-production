fe7db26a53c856797c0ec3fbc27ee3b5b7d5fb5c
Kavin9898 <kavindharan40828@gmail.com>
2026-03-06 16:59:02 +0530

    Update redis.tf

--- infrastructure/redis.tf
+++ infrastructure/redis.tf
@@ -1,6 +1,6 @@
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "redis-cluster"
   engine               = "redis"
-  node_type            = "cache.t2.micro"
+  node_type            = "cache.t3.medium"
   num_cache_nodes      = 1
 }
