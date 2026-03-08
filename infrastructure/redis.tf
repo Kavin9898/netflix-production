de3ba85686177d1e8063414640eec8abae0f5b93
Kavin9898 <kavindharan40828@gmail.com>
2026-03-08 19:37:46 +0530

    Update redis.tf

--- infrastructure/redis.tf
+++ infrastructure/redis.tf
@@ -1,6 +1,19 @@
+resource "aws_elasticache_subnet_group" "redis_subnet" {
+  name = "redis-subnet-group"
+
+  subnet_ids = [
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_2.id
+  ]
+}
+
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "redis-cluster"
   engine               = "redis"
-  node_type            = "cache.t3.medium"
+  node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
+  parameter_group_name = "default.redis7"
+
+  subnet_group_name  = aws_elasticache_subnet_group.redis_subnet.name
+  security_group_ids = [aws_security_group.web_sg.id]
 }
