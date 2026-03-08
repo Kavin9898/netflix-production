ff73b9f4a09b7788156959e64a07b9243d0f3bb2
Kavin9898 <kavindharan40828@gmail.com>
2026-03-08 19:33:19 +0530

    Update cloudfront.tf

--- infrastructure/cloudfront.tf
+++ infrastructure/cloudfront.tf
@@ -1,41 +1,41 @@
-resource "aws_cloudfront_distribution" "cdn" {
+# resource "aws_cloudfront_distribution" "cdn" {
 
-  origin {
-    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
-    origin_id   = "frontend-origin"
-  }
+#   origin {
+#     domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
+#     origin_id   = "frontend-origin"
+#   }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   default_root_object = "index.html"
 
-  default_cache_behavior {
-    target_origin_id       = "frontend-origin"
-    viewer_protocol_policy = "redirect-to-https"
+#   default_cache_behavior {
+#     target_origin_id       = "frontend-origin"
+#     viewer_protocol_policy = "redirect-to-https"
 
-    allowed_methods = ["GET", "HEAD"]
-    cached_methods  = ["GET", "HEAD"]
+#     allowed_methods = ["GET", "HEAD"]
+#     cached_methods  = ["GET", "HEAD"]
 
-    forwarded_values {
-      query_string = false
+#     forwarded_values {
+#       query_string = false
 
-      cookies {
-        forward = "none"
-      }
-    }
-  }
+#       cookies {
+#         forward = "none"
+#       }
+#     }
+#   }
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#     }
+#   }
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
+#   viewer_certificate {
+#     cloudfront_default_certificate = true
+#   }
 
-  tags = {
-    Name = "streamflix-cdn"
-  }
-}
+#   tags = {
+#     Name = "streamflix-cdn"
+#   }
+# }
